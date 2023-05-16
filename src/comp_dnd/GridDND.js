441982fb9620661ef3162b6ca5e86a7db929b6f1
fotomain <foto888999@gmail.com>
2023-05-16 09:54:48 +0300

    1

--- src/comp_dnd/GridDND.js
+++ src/comp_dnd/GridDND.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./styles.css";
 
 // npm i dnd-kit
@@ -11,6 +11,8 @@ import {
     closestCenter,
     KeyboardSensor,
     PointerSensor,
+    TouchSensor,
+    MouseSensor,
     useSensor,
     useSensors,
     DragOverlay
@@ -24,7 +26,34 @@ import {
 
 import SortableItem from "./SortableItem";
 
-const GridDND = () => {
+const device_info = () => {
+
+    var ret = {}
+
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)) {
+        ret.device_is_mobile = true ;
+    } else {
+        ret.device_is_mobile = false ;
+    }
+
+    console.log("=== device_info", device_info)
+
+    return ret;
+}
+
+const GridDND = (props) => {
+
+
+    const init_state={}
+
+    const [state, set_state] = React.useState(init_state);
+
     const [activeId, setActiveId] = useState(null);
     const [items, setItems] = useState([
         "0",
@@ -58,16 +87,37 @@ const GridDND = () => {
         "28",
         "29"
     ]);
-    const sensors = useSensors(
-        useSensor(PointerSensor),
-        useSensor(KeyboardSensor, {
-            coordinateGetter: sortableKeyboardCoordinates
-        })
-    );
+
+    const sensors_mobile = useSensors(
+            useSensor(TouchSensor),
+            // useSensor(MouseSensor),
+            // useSensor(PointerSensor),
+            useSensor(KeyboardSensor, {
+                coordinateGetter: sortableKeyboardCoordinates
+            })
+        );
+
+    const sensors_desktop = useSensors(
+            // useSensor(TouchSensor),
+            // useSensor(MouseSensor),
+            useSensor(PointerSensor),
+            useSensor(KeyboardSensor, {
+                coordinateGetter: sortableKeyboardCoordinates
+            })
+        );
 
     const handleDragStart = (event) => {
         setActiveId(event.active.id);
     };
+
+    window.addEventListener('devicechange', (event) => {
+
+        console.log("=== devicechange ",event)
+        const l_current_device = device_info()
+        set_state({...state,...{device_is_mobile:l_current_device.device_is_mobile}})
+
+    });
+
 
     const handleDragEnd = (event) => {
         setActiveId(null);
@@ -83,39 +133,66 @@ const GridDND = () => {
         }
     };
 
+    window.onresize = function(){
+        console.log("=== window.onresize");
+        const l_current_device = device_info()
+        set_state({...state,...{device_is_mobile:l_current_device.device_is_mobile}})
+
+    }
+    window.onload = function(){
+        console.log("=== window.onload");
+    }
+
+    useEffect(() => {
+
+        console.log("=== onload useEffect")
+        const l_current_device = device_info()
+        set_state({...state,...{device_is_mobile:l_current_device.device_is_mobile}})
+
+        return () => {
+
+        };
+    }, []);
+
+
+
     return (
-        <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            onDragEnd={handleDragEnd}
-            onDragStart={handleDragStart}
-        >
-            <div className="app-intro"
-                // flex={true}
-                // wrap={true}
-                // direction="row"
-                // style={{
-                //     maxWidth: "600px"
-                // }}
+        <>
+            <div>state.device_is_mobile - {(state.device_is_mobile)?'mobile':'desktop'}</div>
+            <br/>
+            <DndContext
+                sensors={(state.device_is_mobile)?sensors_mobile:sensors_desktop}
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+                onDragStart={handleDragStart}
             >
-                <SortableContext items={items} strategy={rectSortingStrategy}>
-                    {items.map((id) => (
-                        <SortableItem key={id} id={id} handle={true} value={id} />
-                    ))}
-                    <DragOverlay>
-                        {activeId ? (
-                            <div
-                                style={{
-                                    width: "100px",
-                                    height: "100px",
-                                    backgroundColor: "red"
-                                }}
-                            ></div>
-                        ) : null}
-                    </DragOverlay>
-                </SortableContext>
-            </div>
-        </DndContext>
+                <div className="app-intro"
+                    // flex={true}
+                    // wrap={true}
+                    // direction="row"
+                    // style={{
+                    //     maxWidth: "600px"
+                    // }}
+                >
+                    <SortableContext items={items} strategy={rectSortingStrategy}>
+                        {items.map((id) => (
+                            <SortableItem key={id} id={id} handle={true} value={id} />
+                        ))}
+                        <DragOverlay>
+                            {activeId ? (
+                                <div
+                                    style={{
+                                        width: "100px",
+                                        height: "100px",
+                                        backgroundColor: "red"
+                                    }}
+                                ></div>
+                            ) : null}
+                        </DragOverlay>
+                    </SortableContext>
+                </div>
+            </DndContext>
+        </>
     );
 };
 
